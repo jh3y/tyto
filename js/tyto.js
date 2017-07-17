956d4ed4d4e6a551167a387c64a96f592e337517
jh3y <jh3y@users.noreply.github.com>
2017-07-17 20:59:29 +0100

    Update 2017-07-17T19:59:16.168Z

--- js/tyto.js
+++ js/tyto.js
@@ -2,7 +2,7 @@
 tyto - http://jh3y.github.io/tyto
 Licensed under the MIT license
 
-jh3y (c) 2016
+jh3y (c) 2017
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -313,7 +313,7 @@ module.exports = { "board": function board(tyto) {
     function print() {
       __p += __j.call(arguments, '');
     }
-    __p += '<div class="tyto-board__options"><button class="mdl-button mdl-js-button mdl-button--icon tyto-board__menu-btn " id="tyto-board__menu" title="Board options"><i class="material-icons">more_vert</i></button><ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect tyto-board__menu mdl-menu--bottom-right" for="tyto-board__menu"><li class="tyto-board__wipe-board mdl-menu__item">Wipe board</li><li class="tyto-board__delete-board mdl-menu__item">Delete board</li><li class="tyto-board__email-board mdl-menu__item">Email board</li><a class="tyto-board__emailer" style="display: none"></a></ul></div><div class="tyto-board__details"><h1 class="tyto-board__title bg--white" contenteditable="true">' + ((__t = tyto.title) == null ? '' : __t) + '</h1>';
+    __p += '<div class="tyto-board__options"><button class="mdl-button mdl-js-button mdl-button--icon tyto-board__menu-btn " id="tyto-board__menu" title="Board options"><i class="material-icons">more_vert</i></button><ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect tyto-board__menu mdl-menu--bottom-right" for="tyto-board__menu"><li class="tyto-board__wipe-board mdl-menu__item">Wipe board</li><li class="tyto-board__delete-board mdl-menu__item">Delete board</li><li class="tyto-board__email-board mdl-menu__item">Email board</li><a class="tyto-board__emailer" style="display: none;"></a></ul></div><div class="tyto-board__details"><h1 class="tyto-board__title bg--white" contenteditable="true">' + ((__t = tyto.title) == null ? '' : __t) + '</h1>';
     if (tyto.boards.length > 1) {
       ;
       __p += '<button class="mdl-button mdl-js-button mdl-button--icon tyto-board__selector__menu-btn " id="tyto-board__selector" title="Select a board"><i class="material-icons">expand_more</i></button><ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect tyto-board__selector__menu mdl-menu--bottom-right" for="tyto-board__selector">';
